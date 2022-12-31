efd4ff23b10220644827b327a8832d35b1ad373d
crejv <87334259+crejv@users.noreply.github.com>
2022-12-31 14:57:56 -0800

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,23 @@
+# Setup terraform provider(s) and version
+# Assume Role
+# Define Variable(s)
+# VPC Proof of Concept Deployment
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = var.custom_role
+  }
+}
+
+variable "custom_role" {
+  description = "arn to assume role [TF_VAR_foo=bar]"
+}
