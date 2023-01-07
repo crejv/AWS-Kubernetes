aebc1f9eb514c9a7c48e1fa1a66f22f94c19072b
crejv <87334259+crejv@users.noreply.github.com>
2023-01-07 15:55:03 -0800

    update

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-provider "aws" {}
-
-provider "aws" {
-  assume_role {
-    role_arn = var.custom_role
-  }
-}
-
-variable "custom_role" {
-  description = "arn to assume role [TF_VAR_foo=bar]"
-}
-
-
 module "kubernetes" {
   source = "./kubernetes"
   vpc_cidr_block = "10.16.0.0/16"
